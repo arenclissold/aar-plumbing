f1aa599fb9ee75f7b8f7344731e807f554491950
Aren Clissold <arenclissold@gmail.com>
2024-02-18 17:54:37 +1100

    add metadata

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const isVercelPreview = process.env.VERCEL_ENV === 'preview'
+const publicHost = isVercelPreview ? process.env.VERCEL_URL : process.env.HOST
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_URL: `${process.env.PROTOCOL}://${publicHost}`,
+  },
+}
 
 module.exports = nextConfig
